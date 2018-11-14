13cc372f189ad8ab2c694b53367db8cde63eb4db
Adrian <Adri98.1mm@gmail.com>
2018-11-14 13:24:16 -0600

    Crear regiones de la imagen

--- src/logic/Controller.java
+++ src/logic/Controller.java
@@ -1,14 +1,23 @@
 package logic;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+import tda.BAsteriskTree;
+import tda.Hash;
+import tda.SimpleList;
+import logic.Tag;
+import logic.WordSample;
 import analysis.AnalyzeText;
 import library.IConstants;
 
 public class Controller implements IConstants{
+	private ArrayList<Tag> ListTags;
 	
 	public Controller() {
-		
+		ListTags = new ArrayList<Tag>();
 	}
 	
 	public void analyzeText(String pPath) {
@@ -37,5 +46,30 @@ public class Controller implements IConstants{
 			numberWords--;
 			number--;
 		}
+	}
+	
+	public ArrayList<Tag> getListTags() {
+		return ListTags;
+	}
+	//Si el hash se ocupa mas adelante crear uno para cada region de lo contrario limpiar
+	public void generateRegions(BufferedImage buffer) {
+		int initialPosX = 0;
+		int initialPosY = 0;
+		int finalPosX = buffer.getWidth()/4;
+		int finalPosY = buffer.getHeight()/4;
+		for(int region = 0; region < NUM_REGIONS; region++) {
+			//extractPixeles(buffer, initialPosX, initialPosY, finalPosX, finalPosY, region);
+			if((region+1)%4 != 0) {
+				initialPosX = finalPosX;
+				finalPosX = finalPosX + buffer.getWidth()/4;
+			}
+			else {
+				initialPosX = 0;
+				finalPosX = buffer.getWidth()/4;
+				initialPosY = finalPosY;
+				finalPosY = finalPosY + buffer.getHeight()/4;
+			}
+		}
+		//assignTags();
 	}
 }
