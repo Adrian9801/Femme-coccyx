ac384d2fbc3335d79bf17a51d1e75fcec985c813
Adrian <Adri98.1mm@gmail.com>
2018-11-21 12:41:49 -0600

    Se agrega el algoritmo de prim

--- src/tda/Grafo.java
+++ src/tda/Grafo.java
@@ -6,12 +6,16 @@ import logic.Sample;
 public class Grafo<T> {
 	private ArrayList<GrafoNode<Nodo<Sample>>> Vertices;
 	private int PosNode;
+	private IQueue<GrafoNode<Nodo<Sample>>> MayorPeso;
+	private IQueue<GrafoNode<Nodo<Sample>>> MenorPeso;
 	private ArrayList<GrafoNode<Nodo<Sample>>> NodosOrigen;
 	private ArrayList<GrafoNode<Nodo<Sample>>> NodosDestino;
 	
 	public Grafo() {
 		Vertices = new ArrayList<GrafoNode<Nodo<Sample>>>();
 		PosNode = 0;
+		MayorPeso = new DoubleList<GrafoNode<Nodo<Sample>>>(false);
+		MenorPeso = new DoubleList<GrafoNode<Nodo<Sample>>>(true);
 		NodosOrigen = new ArrayList<GrafoNode<Nodo<Sample>>>();
 		NodosDestino = new ArrayList<GrafoNode<Nodo<Sample>>>();
 	}
@@ -29,8 +33,10 @@ public class Grafo<T> {
 		GrafoNode<Nodo<Sample>> Node = null;
 		cant = cant/10;
 		for(int i = 0; i < cant; i++) {
+			Node = MayorPeso.dequeue().getValue();
 			if(Node != null)
 				NodosOrigen.add(Node);
+			Node = MenorPeso.dequeue().getValue();
 			if(Node != null)
 				NodosDestino.add(Node);
 		}
@@ -49,11 +55,44 @@ public class Grafo<T> {
 		Vertices.get(pPosOrigen).addArco(Vertices.get(i), pPeso);
 		if(!isAvl) {
 			if(pPeso > 1) {
+				MayorPeso.enqueue(Vertices.get(pPosOrigen), pPeso, pPosOrigen);
+				MenorPeso.enqueue(Vertices.get(pPosOrigen), pPeso, pPosOrigen);
 			}
 		}
 	}
 	
 	public ArrayList<GrafoNode<Nodo<Sample>>> getVertices(){
 		return Vertices;
+	}
+	
+	public void prim() {
+		GrafoNode<Nodo<Sample>> pNodo = Vertices.get(PosNode);
+		GrafoNode<Nodo<Sample>>[] MST = new GrafoNode[Vertices.size()];
+		GrafoNode<Nodo<Sample>> newNode = new GrafoNode<Nodo<Sample>>(pNodo.getValue());
+		IQueue<GrafoNode<Nodo<Sample>>> colaPrioridad = new DoubleList<GrafoNode<Nodo<Sample>>>(true);
+		GrafoNode<Nodo<Sample>>[] visitados = new GrafoNode[Vertices.size()];
+		visitados[0] = pNodo;
+		MST[0] = newNode;
+		pNodo.setVisitado(true);
+		for(int i = 0; i < pNodo.getArcos().size(); i++)
+			colaPrioridad.enqueue(pNodo.getArcos().get(i).getDestino(), pNodo.getArcos().get(i).getPeso(), 0);
+		for(int i = 1; i < Vertices.size() && !colaPrioridad.isEmpty(); i++) {
+			ListDoubleNode<GrafoNode<Nodo<Sample>>> nodo = colaPrioridad.dequeue();
+			while(nodo != null && nodo.getValue().isVisitado())
+				nodo = colaPrioridad.dequeue();
+			if(nodo == null)
+				break;
+			newNode = new GrafoNode<Nodo<Sample>>(nodo.getValue().getValue());
+			visitados[i] = nodo.getValue();
+			nodo.getValue().setVisitado(true);
+			MST[nodo.getPosition()].addArco(newNode, nodo.getPrioridad());
+			MST[i] = newNode;
+			for(int cantArcos = 0; cantArcos < nodo.getValue().getArcos().size(); cantArcos++) {
+				if(!nodo.getValue().getArcos().get(cantArcos).getDestino().isVisitado())
+					colaPrioridad.enqueue(nodo.getValue().getArcos().get(cantArcos).getDestino(), nodo.getValue().getArcos().get(cantArcos).getPeso(), i);
+			}
+		}
+		for(int i = 0; i < Vertices.size(); i++)
+			Vertices.get(i).setVisitado(false);
 	}
 }
